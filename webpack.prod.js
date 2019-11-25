f62075358fef92172996deca0d2629950788ce9c
Paweł Twardziak <ptwardziak@eversis.com>
2019-11-25 13:07:36 +0100

    Init starter

---
+++ webpack.prod.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/index.html', to: './' },
+      { from: './src/favicon.ico', to: './' },
+      { from: './src/assets/fonts', to: './assets/fonts' }
+    ]) 
+]
+});
